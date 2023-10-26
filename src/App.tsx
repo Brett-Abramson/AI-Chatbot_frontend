09f267ad788b0c3132b3525efb41d22a0516d792
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-26 17:21:36 -0500

    Routes setup in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
 
 function App() {
-
   return (
     <main>
-      <h1>Hello World</h1>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
