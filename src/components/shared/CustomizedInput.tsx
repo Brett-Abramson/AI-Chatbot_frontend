a69c5e11699236c313e42678f0c05b723446757b
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-27 13:15:46 -0500

    changed customizedInput color to white

--- src/components/shared/CustomizedInput.tsx
+++ src/components/shared/CustomizedInput.tsx
@@ -12,7 +12,7 @@ const CustomizedInput = (props: Props) => {
     <TextField
       margin="normal"
       InputLabelProps={{ style: { color: "white" } }}
-      InputProps={{ style: { width: "300px", borderRadius: 10, fontSize: 10 } }}
+      InputProps={{ style: { width: "300px", borderRadius: 10, fontSize: 10, color: "white"} }}
       name={props.name}
       label={props.label}
       type={props.type}
