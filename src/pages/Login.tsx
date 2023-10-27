8a931f9f2a818340772cea79f64fd52a3e6514fb
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-27 13:57:26 -0500

    fixed toast loading not dissapearing

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,22 +2,27 @@ import React from "react";
 import { IoIosLogIn } from "react-icons/io";
 import { Box, Button, Typography } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { useAuth } from "../context/AuthContext";
 
 const Login = () => {
   const auth = useAuth();
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const email = formData.get("email") as string;
     const password = formData.get("password") as string;
+
+    toast.loading("Signing In", { id: "login" });
+
     try {
-      toast.loading("Signing In", { id: "login" });
       await auth?.login(email, password);
+      toast.dismiss("login"); // dismiss loading toast
       toast.success("Signed In Succesfully", { id: " login " });
     } catch (error) {
       console.error(error);
+      toast.dismiss("login");
       toast.error("Signing In Failed", { id: "login" });
     }
   };
