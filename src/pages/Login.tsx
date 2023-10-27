67c39553c8936f69ed4cf9ebb9e886ae8956b02d
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-26 20:25:01 -0500

    handleSubmit function added and tested for Login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { IoIosLogIn } from "react-icons/io"
+import { IoIosLogIn } from "react-icons/io";
 import { Box, Button, Typography } from "@mui/material";
 import CustomizedInput from "../components/shared/CustomizedInput";
 
 function Login() {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email")
+    const password = formData.get("password")
+    console.log(email)
+    console.log(password)
+  };
+
   return (
     <Box width={`100%`} height={"100%"} display="flex" flex={1}>
       <Box padding={8} mt={8} display={{ md: "flex", sm: "none", xs: "none" }}>
@@ -19,6 +28,7 @@ function Login() {
         mt={16}
       >
         <form
+          onSubmit={handleSubmit}
           style={{
             margin: "auto",
             padding: "30px",
@@ -59,7 +69,9 @@ function Login() {
                 },
               }}
               endIcon={<IoIosLogIn />}
-            >Login</Button>
+            >
+              Login
+            </Button>
           </Box>
         </form>
       </Box>
