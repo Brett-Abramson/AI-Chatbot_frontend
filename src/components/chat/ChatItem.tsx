706dbaab9bba7089a9fc60c2b396850410e3c9a5
Brett Abramson <brett.m.abramson@gmail.com>
2023-10-30 16:05:36 -0500

    checking API return message for codeblocks and use react-syntax-highlighter

--- src/components/chat/ChatItem.tsx
+++ src/components/chat/ChatItem.tsx
@@ -1,6 +1,31 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import React from "react";
 import { useAuth } from "../../context/AuthContext";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coldarkCold } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+const extractCodeFromString = (message: string) => {
+  if (message.includes("```")) {
+    const blocks = message.split("```");
+    return blocks;
+  }
+};
+
+const isCodeBlock = (str: string) => {
+  if (
+    str.includes("=") ||
+    str.includes(";") ||
+    str.includes("[") ||
+    str.includes("]") ||
+    str.includes("]") ||
+    str.includes("{") ||
+    str.includes("}") ||
+    str.includes("#") ||
+    str.includes("/")
+  ) {
+    return true;
+  }
+};
 
 const ChatItem = ({
   content,
@@ -9,6 +34,7 @@ const ChatItem = ({
   content: string;
   role: "user" | "ai_assistant";
 }) => {
+  const messageBlocks = extractCodeFromString(content);
   const auth = useAuth();
   return role === "ai_assistant" ? (
     <Box sx={{ display: "flex", p: 2, bgcolor: "#004d5612", my: 2, gap: 2 }}>
@@ -16,12 +42,23 @@ const ChatItem = ({
         <img src="openai.png" alt="openai" width={"30px"} />
       </Avatar>
       <Box>
-        <Typography fontSize={"20px"}>{content}</Typography>
+        {!messageBlocks && <Typography fontSize={"20px"}>{content}</Typography>}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={coldarkCold} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography fontSize={"20px"}>{content}</Typography>
+            )
+          )}
       </Box>
     </Box>
   ) : (
     <Box sx={{ display: "flex", p: 2, bgcolor: "#004d56", gap: 2 }}>
-      <Avatar sx={{ ml: "0", bgcolor:"black", color:"white" }}>
+      <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
         {auth?.user?.name[0]}
         {auth?.user?.name.split(" ")[1][0]}
       </Avatar>
